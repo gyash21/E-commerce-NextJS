b3cf1fd78dd3ab9a81d97f4c3c87e93f48ef3186
Yash Gupta <gyash21@gmail.com>
2023-10-08 23:47:11 +0530

    update- added client id and secret

--- app/api/[...nextauth].ts
+++ app/api/[...nextauth].ts
@@ -6,12 +6,13 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  adapter: PrismaAdapter(prisma),
+
 })
 

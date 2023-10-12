8bc6f7918fb5033b8f0fb9c4c7f184bcf516fb0e
Yash Gupta <gyash21@gmail.com>
2023-10-12 23:12:58 +0530

    update- made auth function into an object

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ import Stripe from "stripe"
 
 const prisma = new PrismaClient()
 
-export default NextAuth({
+export const options = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -38,5 +38,7 @@ export default NextAuth({
     },
   },
 
-})
+}
+
+export default NextAuth(options)
 

e1458c7c9e16f643b4bcbfc53a23b22a4449eda5
Yash Gupta <gyash21@gmail.com>
2023-10-18 23:50:58 +0530

    update- user sign in check

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -13,6 +13,16 @@ export default function Nav({user} : Session) {
     return(
         <nav>
             <h1>Styled</h1>
+
+            <ul>
+                <li>Products</li>
+                {/* Checking if the user is Signed In */}
+                {!user && (
+                    <li>
+                    <button onClick={() => signIn()}>Sign </button>
+                    </li>
+                )}
+            </ul>
         </nav>
     )
 }

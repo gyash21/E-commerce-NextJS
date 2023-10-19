323ccf29c63e7576568f98d7f89f8dac19b910ca
Yash Gupta <gyash21@gmail.com>
2023-10-19 22:12:35 +0530

    update- format price on products

---
+++ util/PriceFormat.ts
@@ -0,0 +1,10 @@
+const formatPrice = (amount: number) =>{
+
+    return new Intl.NumberFormat('en-US',{
+       style: 'currency', 
+       currency: 'USD',
+    }).format(amount/100)
+}
+
+
+export default formatPrice

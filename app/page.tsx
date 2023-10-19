bbf11f16c4c9af6ca6041ac8471050a2d7638c20
Yash Gupta <gyash21@gmail.com>
2023-10-19 17:24:48 +0530

    update- rendered product properties

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,26 @@ const getProducts = async() => {
     products.data.map(async (product) => {
 
       const prices = await stripe.prices.list({product: product.id})
+
+      return{
+        id: product.id,
+        name: product.name,
+        price: prices.data[0].unit_amount,
+        image: product.images[0],
+        currency: prices.data[0].currency
+      }
     })
   )
-
+    return productWithPrices
 }
 
 
 
 export default async function Home(){
-  const products = await getProducts();
+  const products = await getProducts()
   return(
     <main>
-      <h1>Hello</h1>
+  
     </main>
   )
 }

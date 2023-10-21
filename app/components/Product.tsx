a31106fee39cab1df8fef381549dfe6e7bc33aef
Yash Gupta <gyash21@gmail.com>
2023-10-22 00:00:00 +0530

    update- changing pathname

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default function Product({name, image, price, id}: {name:string, image:string, price: number | null, id:string}){
 
     return(
-        <Link href={`/product/${id}`}>
+        <Link href={{pathname: '/product/${id}'}, query:{name,image,price,id}>
         <div className="text-gray-700">
         
             <Image src={image} alt={name} width={800} height={800} className="w-96 h-89 object-cover rounded-lg"/>

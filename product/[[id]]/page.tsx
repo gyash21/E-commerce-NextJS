fc094515c11a638326040833f6b700b31da24e6b
Yash Gupta <gyash21@gmail.com>
2023-11-10 22:45:50 +0530

    update- adding description page for product

--- product/[[id]]/page.tsx
+++ product/[[id]]/page.tsx
@@ -1,7 +1,11 @@
-export default async function Product(){
+export default async function Product({ searchParams }){
     return(
-
-        <div>
+        <div className="flex justify-between gap-24 p-12 text-gray-700">
+            <Image src={products.images[0]}
+            alt={Product.name}
+            width={600}
+            height={600}
+            />
         <div>
             <h1>Name</h1>
             <p>Description</p>

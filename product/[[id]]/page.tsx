13f287098d913400c17b05a0c906c3ba28ca5f05
Yash Gupta <gyash21@gmail.com>
2023-11-10 22:50:53 +0530

    update- rendering image from data for products

--- product/[[id]]/page.tsx
+++ product/[[id]]/page.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image"
+
 export default async function Product({ searchParams }){
+
+
     return(
         <div className="flex justify-between gap-24 p-12 text-gray-700">
-            <Image src={products.images[0]}
-            alt={Product.name}
+            <Image src={searchParams.images}
+            alt={searchParams.name}
             width={600}
             height={600}
             />

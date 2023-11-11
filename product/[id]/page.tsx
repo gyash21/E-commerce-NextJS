471f1de27fea363669455e7989678a5db856669f
Yash Gupta <gyash21@gmail.com>
2023-11-11 22:59:41 +0530

    update- directory changes

--- product/[id]/page.tsx
+++ product/[id]/page.tsx
@@ -2,10 +2,9 @@ import Image from "next/image"
 
 export default async function Product({ searchParams }){
 
-
     return(
         <div className="flex justify-between gap-24 p-12 text-gray-700">
-            <Image src={searchParams.images}
+            <Image src={searchParams.image}
             alt={searchParams.name}
             width={600}
             height={600}

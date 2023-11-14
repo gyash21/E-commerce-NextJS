cc2e392535542b127b438cf3c329fcda19f477ec
Yash Gupta <gyash21@gmail.com>
2023-11-14 13:57:25 +0530

    update- adding toggleCart

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -21,13 +21,15 @@ export default function Nav({user} : Session) {
             </Link>
 
             <ul className='flex items-center gap-12'>
-                {/* Checking if the user is Signed In */}
+            {/* Toggle the cart */}
                 <li className='flex items-center text-3xl relative cursor-pointer '>
                 <AiFillShopping/>
                 <span className='bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center'>
                     {cartStore.cart.length}
                 </span>
                 </li>
+
+                      {/* Checking if the user is Signed In */}
                 {!user && (
                     <li className='bg-teal-600 text-white py-2 px-4 rounded-md'>
                     <button onClick={() => signIn()}>Sign </button>

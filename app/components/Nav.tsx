b6f5a2a5ea4bd75f671ce46e0305c91841ffd9af
Yash Gupta <gyash21@gmail.com>
2023-11-14 13:50:14 +0530

    fix- cartStore naming error

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -47,7 +47,7 @@ export default function Nav({user} : Session) {
                         </div>
                 )}
             </ul>
-            {useCartStore.isOpen && <Cart/>}
+            {cartStore.isOpen && <Cart/>}
         </nav>
     )
 }

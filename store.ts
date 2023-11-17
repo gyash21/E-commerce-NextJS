581247fa3d17f0f145ed6977dadf36b7480f5c4b
Yash Gupta <gyash21@gmail.com>
2023-11-17 17:07:17 +0530

    update- remove product function

--- store.ts
+++ store.ts
@@ -18,6 +18,7 @@ type CartState = {
     cart: CartItem[]
     toggleCart: () => void
     addProduct: (item: AddCartType) => void
+    removeProduct: (item: AddCartType) => void
 
 }
 
@@ -42,6 +43,21 @@ export const useCartStore = create<CartState>()(
                     return { cart: updatedCart}
                 } else{
                     return { cart: [...state.cart, {...item,quantity: 1}]}
+                }
+            }),
+            removeProduct: (item) => set((state) => {
+                const existingItem = state.cart.find((cartItem) => cartItem.id === item.id)
+
+                if( existingItem && existingItem.quantity! >1){
+                    const updatedCart = state.cart.map(
+                        (cartItem) => {
+                            if(cartItem.id === item.id){
+                                return {...cartItem, quantity: cartItem.quantity - 1}
+                            }
+                            return cartItem
+                        }
+                    )
+                    return { cart : updatedCart}
                 }
             }),
         }),

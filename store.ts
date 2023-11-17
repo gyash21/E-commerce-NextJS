faa230387543133a8c3daa25913213e46615fc14
Yash Gupta <gyash21@gmail.com>
2023-11-17 14:50:15 +0530

    fix- error in id comparison

--- store.ts
+++ store.ts
@@ -27,14 +27,17 @@ export const useCartStore = create<CartState>()(
             isOpen: false,
             toggleCart: () => set((state) => ({isOpen: !state.isOpen})),
             addProduct: (item) => set((state) => {
-                const existingItem = state.cart.find (cartItem => cartItem === item.id)
+                const existingItem = state.cart.find (cartItem => cartItem.id === item.id)
                 if(existingItem){
                     const updatedCart = state.cart.map((cartItem) => {
-                        if(cartItem === item.id){
+                        if(cartItem.id === item.id){
                             return {...cartItem, quantity: cartItem.quantity + 1}
                         }
                         return cartItem
                     })
+                    return { cart: updatedCart}
+                } else{
+                    return { cart: [...state.cart, {...item,quantity: 1}]}
                 }
             }),
         }),

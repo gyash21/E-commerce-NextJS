6aa34c7ac840eed76330cb45b28f42ce56226e85
Yash Gupta <gyash21@gmail.com>
2023-11-17 14:19:09 +0530

    update- rendering children after hydration

--- app/components/Hydrate.tsx
+++ app/components/Hydrate.tsx
@@ -4,7 +4,18 @@
 import { ReactNode , useEffect, useState} from 'react'
 
 export default function Hydrate({children}:{ children: ReactNode}){ 
+
+    const [isHydrated, setIsHydrated] = useState(false)
+
+    // Wait till Nextjs rehydratrion completes
+
+    useEffect(() => {
+        setIsHydrated(true)
+    }, [])
     return(
 
+        <>
+        {isHydrated? <>children</> : <div>Loading.....</div>}
+        </>
 )
 }
